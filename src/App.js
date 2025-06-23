11f85576d2ab9e7469c280ec69bbf9adb1c97b8e
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-06-23 10:16:42 +0100

    filter quotes by category

--- src/App.js
+++ src/App.js
@@ -9,28 +9,30 @@ import Quotes from "./components/quotes/Quotes";
 function App() {
 const [quotes, setQuotes] = useState([]);
 const [loading, setLoading] = useState(false);
+const [category, setCategory] = useState("All");
 
   // url to the quotes API
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
 
 
+  const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
+
+
     const fetchQuotes = async() => {
 
-      console.log(fetchQuotes);
+      // console.log(fetchQuotes);
       try {
         setLoading(true);
         const request = await fetch(quotesUrl);
-        console.log(request);
+        // console.log(request);
         const results = await request.json();
         setQuotes(results);
       } catch (e) {
-        console.log("error", e);
+        console.log("error found", e);
       }
       setLoading(false);
     };
-
-    
 
 
     useEffect (() => {
@@ -42,14 +44,33 @@ const [loading, setLoading] = useState(false);
       // }, 1000);
       // return () => window.clearInterval(timeInterval);
     },[])
+
+
+    const filteredQuotes = category !== "All" ? quotes.filter((quote) => quote.categories.includes(category)) : quotes;
+
+
+    const handleCategoryChange = (e) => {
+      setCategory(e.target.value)
+    }
+
     
-    console.log(quotes)
+   
 
   return (
     <div className='App'>
       
       <Header />
-      <main>{loading ? <Loader/> : <Quotes quotes={quotes}/>}</main>
+      <main>
+        {loading ? (
+        <Loader/> ) : ( 
+        <Quotes 
+        filteredQuotes={filteredQuotes} 
+        categories={categories} 
+        category={category} 
+        handleCategoryChange={handleCategoryChange}
+        />
+      )}
+        </main>
       <Footer />
     </div>
   );
